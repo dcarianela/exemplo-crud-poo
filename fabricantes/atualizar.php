f5e1e8d86a18c0d74379adca0fd796f46edf64a9
dcarianela <aki.hayakawa5577@gmail.com>
2025-05-13 09:21:45 -0300

    refatora atualizar.php

--- fabricantes/atualizar.php
+++ fabricantes/atualizar.php
@@ -1,11 +1,17 @@
 <?php
-require_once "../src/funcoes-fabricantes.php";
+
+use ExemploCrud\Services\FabricanteServico;
+require_once "../vendor/autoload.php";
 
 /* Obtendo o valor do parâmetro via URL */
 $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
-/* Chamando a função para carregar os dados de um fabricante */
-$fabricante = listarUmFabricante($conexao, $id);
+$fabricanteServico = new FabricanteServico();
+$fabricanteDados = $fabricanteServico->buscarPorId($id);
+
+echo "</pre>";
+var_dump($fabricanteDados);
+echo "</pre>";
 
 /* Verificando se o formulário de atualização foi acionado */
 if (isset($_POST['atualizar'])){
